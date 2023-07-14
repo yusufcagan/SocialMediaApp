64f5380c1fc1ee94c213c23a952deb92be089296
Yusuf Çağan <yusufcagan@outlook.com.tr>
2023-07-14 22:16:35 +0300

    add like  and a few design

--- src/components/PostComponent.tsx
+++ src/components/PostComponent.tsx
@@ -2,39 +2,67 @@ import {
   Text,
   View,
   StyleSheet,
-  SafeAreaView,
+  LayoutChangeEvent,
   Dimensions,
   TouchableOpacity,
   Image,
 } from 'react-native';
-import React from 'react';
-import PIcon from '../assets/images/karizma.jpg';
+import React, {memo, useCallback, useMemo, useState} from 'react';
 import Heart from '../assets/images/heart_space.png';
+import Redheart from '../assets/images/heartIcon.png';
+import CommentIcon from '../assets/images/comment.png';
+import ShareIcon from '../assets/images/again.png';
+import Dm from '../assets/images/dm.png';
 
 const {width, height} = Dimensions.get('window');
 
-const PostData = [
-  {
-    id: 0,
-    name: 'Yusuf',
-    description: 'lorem ipsum let amet.',
-    commet: [],
-    likes: [],
-    profileImage: PIcon,
-  },
-];
+const PostComponent: React.FC<any> = ({data}) => {
+  const [viewHeight, setViewHeight] = useState<number>(0);
+  const [like, setLike] = useState<boolean>(false);
 
-const PostComponent = () => {
+  ////like is very slowly ------------!!!!!!-------------
+  const handleLayout = useCallback(
+    (event: LayoutChangeEvent) => {
+      const {height} = event.nativeEvent.layout;
+      setViewHeight(height);
+      console.log('hL çalıştı');
+    },
+    [viewHeight],
+  );
+
   return (
-    <TouchableOpacity style={styles.postCard}>
-      <View style={{width: width / 10}}>
-        <TouchableOpacity style={{borderRadius: 50, backgroundColor: 'red'}}>
+    <TouchableOpacity onLayout={handleLayout} style={styles.postCard}>
+      <View style={{width: width / 10, alignItems: 'center'}}>
+        <TouchableOpacity style={{borderRadius: 50}}>
           <Image
             resizeMode={'contain'}
-            source={PIcon}
+            source={data.profileImage}
             style={{width: width / 10, height: width / 10, borderRadius: 50}}
           />
         </TouchableOpacity>
+        {data.comment.length !== 0 ? (
+          <>
+            <View
+              style={{
+                marginTop: 5,
+                marginBottom: 5,
+                height: viewHeight - width / 4.5,
+                width: 1,
+                backgroundColor: 'gray',
+              }}
+            />
+            <View>
+              <Image
+                style={{
+                  width: width / 25,
+                  height: width / 25,
+                  borderRadius: 50,
+                }}
+                source={data.comment[0].profileImage}
+              />
+            </View>
+          </>
+        ) : null}
       </View>
       <View style={{marginLeft: width / 25}}>
         <View
@@ -44,7 +72,7 @@ const PostComponent = () => {
             width: (8 * width) / 10,
           }}>
           <TouchableOpacity>
-            <Text style={styles.userNameText}>yusuf.dev</Text>
+            <Text style={styles.userNameText}>{data.userName}</Text>
           </TouchableOpacity>
           <View style={{flexDirection: 'row'}}>
             <Text>22s</Text>
@@ -54,37 +82,55 @@ const PostComponent = () => {
           </View>
         </View>
         <View style={{width: (8 * width) / 10, marginTop: 5}}>
-          <Text style={styles.userNameText}>
-            Lorem ipsum set amet.Lorem ipsum set amet. Lorem ipsum set
-            amet.Lorem ipsum set amet.Lorem ipsum set amet. Lorem ipsum set
-            amet.Lorem ipsum set amet. Lorem ipsum set amet.Lorem ipsum set
-            amet.Lorem ipsum set amet. Lorem ipsum set amet.Lorem ipsum set
-            amet.Lorem ipsum set amet. Lorem ipsum set amet.Lorem ipsum set
-            amet.Lorem ipsum set amet.
-          </Text>
+          <Text style={styles.userNameText}>{data.description}</Text>
           <View style={{flexDirection: 'row'}}>
+            <TouchableOpacity onPress={() => setLike(!like)}>
+              <Image
+                resizeMode="contain"
+                source={like ? Redheart : Heart}
+                style={[
+                  {
+                    width: width / 20,
+                    height: width / 20,
+                    margin: 10,
+                  },
+                  like ? {tintColor: 'red'} : null,
+                ]}
+              />
+            </TouchableOpacity>
             <TouchableOpacity>
               <Image
                 resizeMode="contain"
-                source={Heart}
+                source={CommentIcon}
                 style={{width: width / 20, height: width / 20, margin: 10}}
               />
             </TouchableOpacity>
             <TouchableOpacity>
               <Image
                 resizeMode="contain"
-                source={Heart}
+                source={ShareIcon}
                 style={{width: width / 20, height: width / 20, margin: 10}}
               />
             </TouchableOpacity>
             <TouchableOpacity>
               <Image
                 resizeMode="contain"
-                source={Heart}
+                source={Dm}
                 style={{width: width / 20, height: width / 20, margin: 10}}
               />
             </TouchableOpacity>
           </View>
+          <TouchableOpacity style={{flexDirection: 'row'}}>
+            <Text>
+              {data.comment.length === 0 ? null : data.comment?.length}{' '}
+              {data.comment.length === 0 ? null : 'Yanıt'}
+            </Text>
+            <Text>
+              {'   '}
+              {data.likes.length === 0 ? null : data.likes?.length}{' '}
+              {data.likes.length === 0 ? null : 'Beğenme'}
+            </Text>
+          </TouchableOpacity>
         </View>
       </View>
     </TouchableOpacity>
--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -9,13 +9,43 @@ import {
 } from 'react-native';
 import React from 'react';
 import PostComponent from '../../components/PostComponent';
+import PIcon from '../../assets/images/karizma.jpg';
+import PIcon2 from '../../assets/images/karizma2.jpg';
 
 const {width, height} = Dimensions.get('window');
 
-const HomeScreen = () => {
+const PostData = [
+  {
+    id: 0,
+    userName: 'yusuf.dev',
+    description: 'lorem ipsum let amet.',
+    comment: [{id: 5, userName: 'emirhancagan', profileImage: PIcon2}],
+    likes: [
+      {id: 5, userName: 'emirhancagan', profileImage: PIcon2},
+      {id: 6, userName: 'emir', profileImage: PIcon},
+    ],
+    profileImage: PIcon,
+  },
+  {
+    id: 0,
+    userName: 'emirhancagan',
+    description:
+      'lorem ipsum let amet.lorem ipsum let amet.lorem ipsum let amet.',
+    comment: [
+      {id: 5, userName: 'yusuf.dev', profileImage: PIcon},
+      {id: 6, userName: 'emir', profileImage: PIcon},
+    ],
+    likes: [{id: 5, userName: 'emirhancagan', profileImage: PIcon}],
+    profileImage: PIcon2,
+  },
+];
+
+const HomeScreen: React.FC = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <PostComponent />
+      {PostData.map((item, index) => (
+        <PostComponent key={index} data={item} />
+      ))}
     </SafeAreaView>
   );
 };
